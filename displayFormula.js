27873a1d99dd0e833a2256290bae49d996abae65
raisagennellee <raisagen.lee@gmail.com>
2015-10-26 12:46:34 +0800

    added printing of hyperbola formula

--- displayFormula.js
+++ displayFormula.js
@@ -21,6 +21,23 @@ function printParabolaGeneralForm(h, k, p, orientation, formulaLabel){
 	MathJax.Hub.Queue(["Typeset",MathJax.Hub,formulaLabel]);
 }
 
+function printHyperbolaGeneralForm(h, k, horizontal, vertical, orientation, formulaLabel){
+	var math = document.getElementById(formulaLabel);
+	var xTerm, yTerm;
+	// a si horizontal, b si vertical
+	if (orientation == "vertical"){
+		yTerm = '\\frac{'+assignVertexY(k)+'^2}{'+horizontal+'^2}';
+		xTerm = '\\frac{'+assignVertexX(h)+'^2}{'+vertical+'^2}';
+		math.innerHTML = '\\[' + yTerm + '-' + xTerm +'=1\\]';
+	}
+	else if (orientation == "horizontal"){
+		yTerm = '\\frac{'+assignVertexY(k)+'^2}{'+vertical+'^2}';
+		xTerm = '\\frac{'+assignVertexX(h)+'^2}{'+horizontal+'^2}';
+		math.innerHTML = '\\[' + xTerm + '-' + yTerm +'=1\\]';
+	}
+	MathJax.Hub.Queue(["Typeset",MathJax.Hub,formulaLabel]);
+}
+
 function printMatrix(points, formulaLabel){
 	var math = document.getElementById(formulaLabel);
 	var matrixContent = '$$\\begin{bmatrix}';
